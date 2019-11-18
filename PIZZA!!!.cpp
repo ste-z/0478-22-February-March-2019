27b132566f310e4006f960d78bcaf75d98a45ddc
Incrediblez7 <william.zhang1093@gmail.com>
2019-11-18 09:46:26 +0800

    Update PIZZA!!!.cpp

--- PIZZA!!!.cpp
+++ PIZZA!!!.cpp
@@ -4,6 +4,9 @@
 #include <stdio.h>
 using namespace std;
 int ICT_Pizza_data[10000][5];
+string toppingstring[7] = {0, "Pepperoni", "Chicken", "Extra Cheese", "Mushrooms", "Spinach", "Olives"};
+string sizestring[4] = {0, "Small", "Medium", "Big"};
+string thicknessstring[3] = {0, "Thin", "Thick"};
 void ICT_Pizza_QUIT(int seq)
 {
     int top[7]={0},size[4]={0},thickness[3]={0};
@@ -24,34 +27,36 @@ void ICT_Pizza_QUIT(int seq)
     cout<<"Thick THIN orders:"<<thickness[1]<<"("<<100.0*thickness[1]/totalThick<<"%)"<<endl;
     cout<<"Thick THICK orders:"<<thickness[2]<<"("<<100.0*thickness[2]/totalThick<<"%)"<<endl;
     for (int i=1; i<=6; i++) {
-        cout<<"Topping "<<i<<" orders:"<<top[i]<<"("<<100.0*top[i]/totalTop<<"%)"<<endl;
+        cout<<"Topping "<<toppingstring[i]<<" orders:"<<top[i]<<"("<<100.0*top[i]/totalTop<<"%)"<<endl;
     }
     cout<<endl;
 }
 
 void ICT_Pizza_1()
 {
+    cout<<"PIZZA PROGRAM \n";
     int input1,input2,input3;
     int topping[3];
     int exec;
     int sequenceNum=0;
     while (true) {
         memset(topping, 0, sizeof(topping));
-        cout<<"SIZE from 1-3: ";
+        cout<<"Please? Select pizza SIZE (1: Small, 2: Medium, 3: Large): ";
         cin>>input1;
-        cout<<"CRUST THICKNESS from 1-2: ";
+        cout<<"Please? Select pizza CRUST thickness (1: Thin, 2: Thick): ";
         cin>>input2;
-        cout<<"TOPPING COUNT From 1-3: ";
+        cout<<"Please? Select pizza Topping required (from 1 to 3): ";
         cin>>input3;
+        cout<<"Topping Menu:\n1. Pepperoni\t2. Chicken\t3. Extra Cheese\n4. Mushrooms\t5. Spinach\t6. Olives
         for (int i=0; i<input3; i++) {
-            cout<<"TOPPING "<<i+1<<" from 1-6: ";
+            cout<<"Select topping "<<i+1<<" (enter 1-6): ";
             cin>>topping[i];
         }
-        cout<<input1<<" "<<input2<<" ";
+        cout<<sizestring[input1]<<" "<<thicknessstring[input2]<<" ";
         for (int i=0; i<input3; i++) {
-            cout<<topping[i]<<" ";
+            cout<<toppingstring[topping[i]]<<" ";
         }
-        cout<<endl<<"Correct? (Yes=1,No=2,Quit=3)";
+        cout<<endl<<"Confirm? (Yes=1,No=2,Quit=3)";
         cin>>exec;
         if (exec==1) {
             ICT_Pizza_data[sequenceNum][0]=input1;
